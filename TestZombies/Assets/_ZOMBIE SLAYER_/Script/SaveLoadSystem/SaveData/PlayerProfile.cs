4e3fad21f66031c8dd7553dc128c8601c8cd4a61
Vladislav-Oleynik <kaktuskolushiy@gmail.com>
2022-12-24 23:05:54 +0200

    Added prototype of a local save/load system

---
+++ TestZombies/Assets/_ZOMBIE SLAYER_/Script/SaveLoadSystem/SaveData/PlayerProfile.cs
@@ -0,0 +1,15 @@
+﻿namespace SaveData
+{
+    [System.Serializable]
+    public class PlayerProfile 
+    {
+        public int id;
+        public string name;
+        public int balance;
+
+        public PlayerProfile() 
+        {
+            name = "Player";
+        }
+    }
+}
