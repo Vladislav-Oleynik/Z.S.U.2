f9161c4972fd8d53c48cbe277461d9784bde4096
Vladislav-Oleynik <kaktuskolushiy@gmail.com>
2023-01-20 23:15:29 +0200

    Add weapon icon change feature

--- TestZombies/Assets/_ZOMBIE SLAYER_/Script/ItemsContainer.cs
+++ TestZombies/Assets/_ZOMBIE SLAYER_/Script/ItemsContainer.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemsContainer : MonoBehaviour
 {
@@ -11,6 +12,10 @@ public class ItemsContainer : MonoBehaviour
 
     public static List<CustomWeapon> firstWeaponsList, secondWeaponsList;
 
+    public Image firstWeapon, secondWeapon;
+
+    private int firstWeaponIdx, secondWeaponIdx;
+
     private void Start()
     {
         head_accsList = head_accs;
@@ -21,6 +26,59 @@ public class ItemsContainer : MonoBehaviour
         feetList = feet;
         firstWeaponsList = firstWeapons;
         secondWeaponsList = secondWeapons;
+        firstWeaponIdx = secondWeaponIdx = 0;
+
+        firstWeapon.sprite = firstWeapon.sprite;
+        secondWeapon.sprite = secondWeapon.sprite;
+
+        if (firstWeaponsList.Count != 0)
+            firstWeapon.sprite = firstWeaponsList[0].icon;
+        else
+            Debug.Log("firstWeaponsList.Count == 0");
+
+        if (secondWeaponsList.Count != 0)
+            secondWeapon.sprite = secondWeaponsList[0].icon;
+        else
+            Debug.Log("secondWeaponsList.Count == 0");
     }
 
+    public void ChangeWeaponImage(bool isFirstWeapon = false)
+    {
+        if (firstWeaponsList.Count == 0 || secondWeaponsList.Count == 0)
+        {
+            Debug.Log("First or second weapons list is empty!");
+            return;
+        }
+
+        if (isFirstWeapon)
+        {
+            firstWeaponIdx++;
+
+            if (firstWeaponIdx < firstWeapons.Count)
+            {
+                firstWeapon.sprite = firstWeaponsList[firstWeaponIdx].icon;
+            }
+            else
+            {
+                firstWeapon.sprite = firstWeaponsList[0].icon;
+                firstWeaponIdx = 0;
+            }
+            
+        }
+        else
+        {
+            secondWeaponIdx++;
+
+            if (secondWeaponIdx < secondWeapons.Count)
+            {
+                secondWeapon.sprite = secondWeaponsList[secondWeaponIdx].icon;
+            }
+            else
+            {
+                secondWeapon.sprite = secondWeaponsList[0].icon;
+                secondWeaponIdx = 0;
+            }
+            
+        }
+    }
 }
